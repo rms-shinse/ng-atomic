d5713c424f74c03b3359f1facf5087b70024ce58
rms-shinse <shinse_tanaka@recruit-ms.co.jp>
2020-02-29 00:44:40 +0900

    add plain ProfilePage

--- projects/firstApp/src/app/app-routing.module.ts
+++ projects/firstApp/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ProfilePageComponent } from "./pages/profile-page/profile-page.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "profile",
+    pathMatch: "full"
+  },
+  {
+    path: "profile",
+    component: ProfilePageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
